1074d4f5fd48ce6779ef04d140ec23c979c8ad36
Brady Leavitt <brady.d.leavitt@gmail.com>
2025-11-05 14:40:34 -0500

    Adds linting and unit tests

--- src/model/train.py
+++ src/model/train.py
@@ -59,7 +59,9 @@ def split_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(
+        X_train, y_train
+    )
 
 
 def parse_args():
@@ -68,7 +70,9 @@ def parse_args():
 
     # add arguments
     parser.add_argument("--training_data", dest="training_data", type=str)
-    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
+    parser.add_argument(
+        "--reg_rate", dest="reg_rate", type=float, default=0.01
+    )
 
     # parse args
     args = parser.parse_args()
